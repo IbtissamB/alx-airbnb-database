9d44208890cf065309e82634138e9692e6754f20
Ibtissam <77987916+IbtissamB@users.noreply.github.com>
2025-07-06 22:33:12 +0300

    Create subqueries.sql

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,16 @@
+SELECT * FROM Property 
+WHERE property_id IN (
+    -- Subquery: Get property_ids with AVG(rating) > 4.0
+    SELECT property_id 
+    FROM Review 
+    GROUP BY property_id 
+    HAVING AVG(rating) > 4.0  
+); 
+
+SELECT * FROM User 
+WHERE user_id IN (
+    SELECT user_id 
+    FROM Booking 
+    GROUP BY user_id 
+    HAVING COUNT(*) > 3  
+);
