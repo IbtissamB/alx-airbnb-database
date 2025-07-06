fb851ba8490d8eebf76b57a2c9d2b25dded2848f
Ibtissam <77987916+IbtissamB@users.noreply.github.com>
2025-07-06 22:29:03 +0300

    Update joins_queries.sql

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,26 @@
+-- a query using an INNER JOIN to retrieve all bookings and the respective users who made those bookings.
+
+SELECT * FROM Bookings 
+INNER JOIN Users 
+ON Bookings.user_id = Users.user_id; 
+
+-- a query using a LEFT JOIN to retrieve all properties and their reviews, including properties that have no reviews.
+SELECT 
+    Property.property_id,
+    Property.name,
+    Property.location,
+    Property.pricepernight,
+    Review.review_id,
+    Review.rating,
+    Review.comment,
+    Review.created_at AS review_date
+FROM 
+    Property
+LEFT JOIN 
+    Review ON Property.property_id = Review.property_id
+
+-- a query using a FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user.
+
+  SELECT * FROM Users 
+FULL OUTER JOIN Bookings 
+ON Users.user_id = Bookings.user_id 
