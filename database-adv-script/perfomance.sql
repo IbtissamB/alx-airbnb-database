4977d0763b76d81509808e78df372d11f4ccee0f
Ibtissam <77987916+IbtissamB@users.noreply.github.com>
2025-07-07 00:09:52 +0300

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,3 +1,4 @@
+Initial Query:
 SELECT
     -- Booking Details
     b.booking_id,
@@ -33,3 +34,31 @@ INNER JOIN
     Properties AS p ON b.property_id = p.property_id -- Join with Properties table on property_id
 INNER JOIN
     Payments AS pay ON b.payment_id = pay.payment_id; -- Join with Payments table on payment_id
+
+-- For PostgreSQL:
+EXPLAIN ANALYZE
+SELECT
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price AS booking_total_price,
+    b.status AS booking_status,
+    u.user_id,
+    u.username,
+    u.email AS user_email,
+    p.property_id,
+    p.property_name,
+    p.location,
+    p.price_per_night,
+    pay.payment_id,
+    pay.amount AS payment_amount,
+    pay.payment_date,
+    pay.status AS payment_status
+FROM
+    Bookings AS b
+INNER JOIN
+    Users AS u ON b.user_id = u.user_id
+INNER JOIN
+    Properties AS p ON b.property_id = p.property_id
+INNER JOIN
+    Payments AS pay ON b.payment_id = pay.payment_id;
